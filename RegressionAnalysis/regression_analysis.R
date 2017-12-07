25296b3e6c552e563ece1166f1f12fbccf73a16a
CiaranWelsh <c.welsh2@newcastle.ac.uk>
2017-12-07 20:16:54 +0000

    begin time delay regression

--- RegressionAnalysis/regression_analysis.R
+++ RegressionAnalysis/regression_analysis.R
@@ -1,6 +1,7 @@
 library(limma)
 library(reshape2)
 library(tidyr)
+library(dplyr)
 library(gtools)
 library(ggplot2)
 library(utils)
@@ -17,8 +18,10 @@ library(Rmisc)
 ## Same as in the other scripts
 
 ## working directory for analysis 
-dire = '/home/b3053674/Documents/LargeStudy/Limma'
-regression_dire = '/home/b3053674/Documents/LargeStudy/RegressionAnalysis'
+# dire = '/home/b3053674/Documents/LargeStudy/Limma'
+# regression_dire = '/home/b3053674/Documents/LargeStudy/RegressionAnalysis'
+dire = 'C:\\Users\\Ciaran\\Documents\\large_study\\Limma'
+regression_dire = 'C:\\Users\\Ciaran\\Documents\\large_study\\RegressionAnalysis'
 setwd(regression_dire)
 
 ## file containing delta Ct values
@@ -74,90 +77,13 @@ data$id = NULL
 
 ## transpose to get samples along the columns and genes down the rows
 data = data.frame(data)
+head(data)
+
+
 ################################################################
 ##                                                            ##   
-##        Plot COL1A1 against CTGF. Correlated?               ##
+##    Correlate Two Variables using linear regression         ##
 ##                                                            ##
-################################################################
-
-
-
-##isolate CTGF and COL1A1
-col_vs_ctgf_data = t(data[c('CTGF', 'COL1A1'),])
-
-## Look at data
-head(col_vs_ctgf_data)
-
-## split id into separate columns in dataframe
-split = data.frame(str_split_fixed(rownames(col_vs_ctgf_data),'_', 4))
-
-## row and col names
-rownames(split) = rownames(col_vs_ctgf_data)
-head(split)
-colnames(split) = c('cell_line', 'treatment', 'time', 'replicate')
-
-## merge split frame into col_vs_ctgf_data
-col_vs_ctgf_data = merge(col_vs_ctgf_data, split, by=0, all=T) 
-
-dim(col_vs_ctgf_data)
-head(col_vs_ctgf_data)
-
-
-## create function for calculating r2
-reg = function(df){
-  reg_fun = lm(formula=df$COL1A1 ~ df$CTGF)
-  slope = round(coef(reg_fun)[2], 3)
-  r2 = round(as.numeric(summary(reg_fun)[8]), 3)
-  r2.adj = round(as.numeric(summary(reg_fun)[9]), 3)
-  intercept = round(coef(reg_fun)[1], 3)
-  c(slope, intercept, r2, r2.adj)
-}
-
-## calculate regression data and relabel cols
-reg_data = ddply(col_vs_ctgf_data, c("treatment", 'cell_line'), reg)
-reg_data
-colnames(reg_data) = c('treatment', 'cell_line', 'slope', 'intercept', 'r2', 'r2.adj')
-
-## plot data 
-ggplot(col_vs_ctgf_data, 
-       aes(x=CTGF, y=COL1A1, group=cell_line, color=treatment)) +
-  geom_point() +
-  
-  ## use black and white theme as base
-  theme_bw() + 
-  theme(strip.text.x = element_text(size=16), 
-        strip.text.y = element_text(size=16), 
-        plot.title = element_text(hjust = 0.5, size=22),
-        axis.text = element_text(size=10),
-        axis.text.x = element_text(size=14), 
-        axis.text.y = element_text(size=14)) + 
-  
-  ## add regression line with standard error
-  geom_smooth(method=lm, se=T) +
-  
-  ## add title
-  ggtitle('Linear Regression. x=CTGF, y=COL1A1') +
-  
-  ## convert to facet grid to plot cell lines against treatments
-  facet_grid(cell_line ~ treatment, scales = 'fixed') + 
-  
-  ## add equations for labels
-  geom_label(data=reg_data, 
-             inherit.aes = F, 
-             aes(x=4, y=45, 
-                 label=paste('y=', 
-                             intercept,"+", 
-                             slope, "x, r2=", r2))) +
-  labs(x='CTGF (DeltaCt)', y='COL1A1 (DeltaCt)') 
-# guide_legend(name='Treatment')
-
-
-
-################################################################
-##                                                            ##   
-##            Based on correlation of CTGF and COL1A1         ##
-##            make general function to do same with any gene  ##
-##            pair                                            ##
 ################################################################
 
 ## create function for calculating r2
@@ -231,6 +157,8 @@ corr_plot = function(data, x, y, eq_loc=c(5, 20)){
                                slope, "x, r2=", r2),
                    size=6)) +
     labs(x=paste(x, '(DeltaCt)'), y=paste(y, '(DeltaCt)'))
+  
+  # reg_data#[, order('r2')])
 }
 
 
@@ -245,7 +173,78 @@ corr_plot = function(data, x, y, eq_loc=c(5, 20)){
 # [64] "TGFBR2"   "THBS1"    "THBS2"    "TIMP1"    "TIMP3"    "TP53BP1"  "VCAN"     "VEGFA"    "VIM" 
 
 
-corr_plot(data, 'ELN', 'COL1A1', eq_loc=c(1, 40))
+corr_plot(data, 'HAS2', 'COL1A1', eq_loc=c(1, 40))
+
+
+##############################################
+##                                          ##
+##    Correlation with time delay           ##
+##                                          ##
+##############################################
+
+
+
+
+
+
+## steps
+## 1) isolatetwo variables of interest (COL1A1 and CTGF for instance)
+## 2) Interpolate the data using cubic spline for example. specify resolution.
+## 3) add time delay to the predictor variable
+## 4) Calculate pearsons coef. Also build in mutual information for comparison
+## 5) Find a way of maximizing pearsons or MI accross range of time delay
+
+
+
+# time_delay_corr = function(data, x, y, t){
+x = 'CTGF'
+y = 'COL1A1'
+df = data[, c(x, y)]
+
+## split id into separate columns in dataframe
+split = data.frame(str_split_fixed(rownames(data),'_', 4))
+
+## row and col names
+rownames(split) = rownames(df)
+head(split)
+colnames(split) = c('cell_line', 'treatment', 'time', 'replicate')
+
+## merge split frame into col_vs_ctgf_data
+merged = merge(df, split, by=0, all=T)
+
+merged$time = factor(merged$time)
+merged
+for (c in levels(merged$cell_line)){
+  for (t in levels(merged$time)){
+    print(merged[,(merged$cell_line == c & merged$time == t)])
+  }
+}
+
+# time_delay_corr(data, 'CTGF', 'COL1A1', 60)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
